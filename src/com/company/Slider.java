44975a5a49ffe93e41ee60b29183cfb045351e2a
spoose <spooseageese@gmail.com>
2021-12-03 21:20:29 -0800

    Update Slider.java
    change canvas to drawDemo type to draw box

--- src/com/company/Slider.java
+++ src/com/company/Slider.java
@@ -6,18 +6,25 @@ import javax.swing.event.ChangeListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 class Slider extends JSlider implements ChangeListener {
 
-    private JLabel canvas;
+    private drawDemo canvas;
     private final JLabel status;
     private final String format;
     private ArrayList<File> data;
 
-    Slider(JLabel status, String format, ArrayList<File> video) {
+    // private int currentFrame;
+    private int currentFrame;
+    private Map<Integer,drawDemo> video_ori_map;
+
+    Slider(JLabel status, String format, ArrayList<File> video, Map<Integer,drawDemo>video_ori_map) {
         super();
         this.status = status;
         this.format = format;
+        this.video_ori_map = video_ori_map;
         reset(video);
         addChangeListener(this);
     }
@@ -50,17 +57,35 @@ class Slider extends JSlider implements ChangeListener {
         setPaintLabels(true);
     }
 
-    public void setCanvas(JLabel canvas) {
-        this.canvas = canvas;
+    public void setCanvas(drawDemo canvas) {
+        this.canvas = (drawDemo) canvas;
+    }
+
+
+    public int getCurrentFrame() {
+        return currentFrame;
     }
 
     @Override
     public void stateChanged(ChangeEvent e) {
-        status.setText(String.format(format, getValue() + 1));
+        System.out.println("--------- slider change---------");
+        status.setText("Value of the slider is: " + ((JSlider)e.getSource()).getValue());
+        currentFrame = ((JSlider)e.getSource()).getValue();
+
+        // status.setText(String.format(format, getValue() + 1));
         if (canvas != null) {
             BufferedImage newImage = ImageReader.getInstance().BImgFromFile(data.get(getValue()));
+
+            drawDemo temp_video_ori = new drawDemo();
+            if (video_ori_map.containsKey(currentFrame)){
+                 System.out.println("video_ori_map contains:"+currentFrame+"'s frame"+", the map size: "+video_ori_map.size());
+                 temp_video_ori = video_ori_map.get(currentFrame);
+             }
+            canvas.shapes =temp_video_ori.shapes;
             canvas.setIcon(new ImageIcon(newImage));
+            video_ori_map.put(currentFrame,temp_video_ori);
             canvas.repaint();
+            System.out.println("--------- slider change-end--------");
         }
     }
 }
